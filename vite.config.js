283ecddc0d87a9a2f7b172133514d1986ee864bb
ThiagoGuilherme71 <thiagoguilherme.barbosaa@gmail.com>
2025-12-13 15:41:02 -0300

    config to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/my-portfolio/",
   plugins: [react()],
+  base: '/', // Para Vercel use '/', para GitHub Pages use '/my-portfolio/'
 })
